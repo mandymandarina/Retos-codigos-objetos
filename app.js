f2937c54a259ea87fc6c613446b9066c578f2d11
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-06-19 21:16:23 -0400

    se agrego ejercicios

--- app.js
+++ app.js
@@ -50,4 +50,52 @@ function removeStringValuesLongerThan(num, obj) {
 var output = removeStringValuesLongerThan(6, obj);
 console.log(output);
 
+//ejercicio 4
+//Escriba una función llamada getElementOfArrayProperty.Dado un objeto, una clave y un índice numérico, getElementOfArrayProperty devuelve el valor del elemento en el índice dado del arreglo localizado dentro del objeto dado en la clave dada.Notas:Si el arreglo está vacío, debería devolver undefined.Si el índice dado está fuera de rango del arreglo ubicado en la clave dada, debería devolver undefined.Si la propiedad en la clave dada no es un arreglo, debería devolver undefined.Si no hay ninguna propiedad en la clave, debería devolverse undefined.
 
+var obj = {
+  key: ['Jamil', 'Albrey']
+ };
+
+function getElementOfArrayProperty(obj, key, index) {
+ 
+  return obj[key] [index];
+}
+
+var output = getElementOfArrayProperty(obj, 'key', 0);
+console.log(output);
+
+//ejercicio 5
+//Escribe una función llamada greetCustomer.Dando un nombre, greetCustomer devuelve un saludo basado en cuántas veces ese cliente ha visitado el restaurante. Consulte el objeto customerData.El saludo debe ser diferente, dependiendo del nombre de la reservación.
+const customerData = {
+  Joe: {
+    visits: 1,
+  },
+  Carol: {
+    visits: 2,
+  },
+  Howard: {
+    visits: 3,
+  },
+  Carrie: {
+    visits: 4,
+  },
+};
+
+function greetCustomer(firstName) {
+  var greeting = '';  
+  if (customerData[firstName] === 0){
+    greeting ='Welcome! Is this your first time?';
+  } else if(customerData[firstName].visits === 1){
+    greeting ="Welcome back, Joe! We're glad you liked us the first time!";
+  }else if (customerData[firstName].visits >1){
+    greeting ='Welcome back, Carol! So glad to see you again!';
+  }
+  return greeting;
+}
+
+var output = greetCustomer('Carol');
+console.log(output);
+
+//ejercicio 6
+  
